17ca90171ea15ae1b5d0fe453747c322bddb6fcd
ovoitovych <alexandr.v1111@gmail.com>
2021-08-14 02:37:44 +0300

    language-model was created

---
+++ codeArena/language/models.py
@@ -0,0 +1,9 @@
+# from django.db import models
+from djongo import models
+
+# Create your models here.
+class Language(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
