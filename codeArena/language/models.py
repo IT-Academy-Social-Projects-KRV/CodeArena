b6c687d70b7caff1507c0f556673e56d96ba1d70
alexandrv1111 <56887566+alexandrv1111@users.noreply.github.com>
2021-08-16 12:35:24 +0300

    Update models.py

--- codeArena/language/models.py
+++ codeArena/language/models.py
@@ -3,7 +3,7 @@ from djongo import models
 
 # Create your models here.
 class Language(models.Model):
-    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
