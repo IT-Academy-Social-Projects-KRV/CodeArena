3effbe78027791b14da2a506dbc6f906b5da0639
Oleksandr Havron <43618379+OleksandrHavron@users.noreply.github.com>
2021-08-20 19:29:12 +0300

    Update mongo-init.sh

--- bin/mongo-init.sh
+++ bin/mongo-init.sh
@@ -1,11 +1,11 @@
 db.createUser(
 	{
-		user: "codearena",
-		pwd: "password",
+		user: ${MONGODB_USER},
+		pwd: ${MONGODB_USER_PASS},
 		roles: [
 			{
 				role: "dbOwner",
-				db: "codearena_mdb"
+				db: ${MONGODB_NAME}
 			}
 					                ]
 			        }
