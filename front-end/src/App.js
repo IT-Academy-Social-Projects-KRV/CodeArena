382518ce5ccee830f9dadd18f4e3eaf4f6894c24
Maksson <maxqwew@gmail.com>
2021-08-27 12:12:36 +0300

    Title was changed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import Header from '../src/Components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../src/Components/Footer/Footer';
 import MainContainer from '../src/Components/HomePage/MainContainer';
+import Auth_Header from '../src/Components/Auth_Header/Auth_Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <MainContainer/>
-      <Footer/>
-    </div>
+    <>
+      <Auth_Header />
+      <li className='App'>
+        <MainContainer />
+        <Footer />
+      </li>
+    </>
   );
 }
 
