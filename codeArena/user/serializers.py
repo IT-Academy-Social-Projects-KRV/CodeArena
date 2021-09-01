9fc3ce0dadcdf6badcdd936c013f1d427483ad20
dmytrosmetanin <dmytro.smetanin@gmail.com>
2021-09-02 00:56:15 +0300

    Created serializers for User and Coder tables

---
+++ codeArena/user/serializers.py
@@ -0,0 +1,36 @@
+from django.db import models
+from rest_framework import serializers
+from user.models import User, Coder
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'nickname', 'first_name', 'last_name',
+                  'password', 'role_id', 'created_at', 'updated_at', 'status']
+
+
+def create(self, validated_data):
+    if "password" in validated_data:
+        from django.contrib.auth.hashers import make_password
+        validated_data["password"] = make_password(validated_data["password"])
+    return super().create(validated_data)
+
+
+def update(self, instance, validated_data):
+    if "password" in validated_data:
+        from django.contrib.auth.hashers import make_password
+        validated_data["password"] = make_password(validated_data["password"])
+    return super().update(instance, validated_data)
+
+
+class CoderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coder
+        fields = ['id', 'city', 'description', 'phone_number', 'level_id']
