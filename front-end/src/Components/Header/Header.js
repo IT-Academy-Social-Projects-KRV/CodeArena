c9a74bf0baa93d82ee22b5352a1d3b7f6e4a4f53
ku4er.marina@gmail.com <ku4er.marina@gmail.com>
2021-09-01 21:15:23 +0300

    Router of pages was added

--- front-end/src/Components/Header/Header.js
+++ front-end/src/Components/Header/Header.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import Navigation from './Navigation';
 import './header.css';
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom"
+import { Nav } from 'react-bootstrap';
 
-function Header () {
+
+import MainContainer from '../pages/homePage/MainContainer';
+import NewsPage from '../pages/newsPage/NewsPage';
+
+function Header() {
     return (
-        <header className="header">
-            <section className="top_menu">
-                <div>
-                    <button>Sign Up</button>
-                    <button>Log In</button>
+        <>
+            <header className="header type">
+                <section className="top_menu">
+                    <div>
+                        <button>Sign Up</button>
+                        <button>Log In</button>
+                    </div>
+                </section>
+                <div className="mainheader type">
+                <h3><b><nav className="main_navigation type" font-size="2">
+                        <a href="/">Code Arena</a>
+                    </nav></b></h3>
+                    <Navigation />
                 </div>
-            </section>
-            <div className="mainheader">
-                <div>Code Arena</div>
-                <Navigation />
-            </div>
-        </header>
+            </header>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={MainContainer} />
+                    <Route exact path="/news" component={NewsPage} />
+                </Switch>
+            </Router>
+        </>
     )
+
 }
 
 export default Header;
