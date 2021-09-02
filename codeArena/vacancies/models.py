0366a511ad11f656475b785a0bf2774faf10f219
Viktorika-p <vika.akado@gmail.com>
2021-09-02 15:49:50 +0300

    fix table Vacancies

--- codeArena/vacancies/models.py
+++ codeArena/vacancies/models.py
@@ -1,30 +1,21 @@
-from codeArena.user.models import Coder
+from user.models import User
 from django.db import models
-from django.db.models.fields import CharField, DateField, FloatField, IntegerField, EmailField, DateTimeField, ImageField
-from user.enums import UserStatus
-from django.utils.timezone import now
-
 
 
 class Vacancies(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=250)
-    recruiter_id = models.IntegerField(max_length=250)
-    company = models.CharField(max_length=20)
-    created_at = models.DateTimeField(default=now, editable=False)
-    updated_at = models.DateTimeField(null=True, blank=True)
-    language_id = models.IntegerField(max_length=20)
-    company_logo = models.ImageField(Null=True)
+    recruiter_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at =  models.DateTimeField(auto_now=True)
+    language_id = models.IntegerField()
+    company_logo = models.ImageField(blank=True, null=True)
+    coder_id = models.ManyToManyField(User, related_name="coder")
 
     def __str__(self):
-        return f'{self.name}, {self.description}, {self.recruiter_id}, {self.company}, {self.created_at}, {self.updated_at}, {self.language_id}, {self.company_logo}'
+        return f'{self.name}, {self.description}, {self.recruiter_id}, {self.company}, \
+            {self.created_at}, {self.updated_at}, {self.language_id}, {self.company_logo}, {self.coder_id}'
 
 
 
-
-class Coder_vacancies(models.Model):
-    vacancies_id = models.ForeignKey(Vacancies, on_delete=models.CASCADE)
-    coder_id = models.ForeignKey(Coder, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f'{self.vacancies_id}, {self.coder_id}'
