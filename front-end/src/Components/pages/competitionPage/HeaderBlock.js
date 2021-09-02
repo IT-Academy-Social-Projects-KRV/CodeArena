8edaf4a0f91933d83b6f053aa07291c60338e210
Maksson <maxqwew@gmail.com>
2021-09-03 00:20:03 +0300

    Folder and files of competition page was added

---
+++ front-end/src/Components/pages/competitionPage/HeaderBlock.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Component } from 'react';
+import { Card, } from 'react-bootstrap';
+import  '../../../App.css';
+
+class HeaderBlock extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { card } = this.props
+        return (
+            <>
+            <div className="blue">
+                <Card.Header >
+                    
+                    <h2 >{card.name} </h2>
+                    
+                </Card.Header>
+                </div>
+            </>
+        )
+    }
+}
+
+export default HeaderBlock
---
+++ front-end/src/Components/pages/competitionPage/MiddleBlock.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Component } from 'react';
+
+class MiddleBlock extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { card } = this.props
+        return (
+            <>
+                <Card bg="light" style={{ width: '54.6rem' }} >
+                    <Card.Body style={{ height: '17rem' }}>
+                        <Card.Title className="text-center type">
+                            Description of this competition
+                        </Card.Title>
+                        <Card.Text > <p> {card.description}  </p> </Card.Text>
+                    </Card.Body>
+                </Card>
+            </>
+        )
+    }
+}
+
+export default MiddleBlock
