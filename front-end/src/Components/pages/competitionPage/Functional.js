7b1e37238256cd23fca7039dbe58f2f4fc10b521
Maks Kovalchuk <87645543+Maksson@users.noreply.github.com>
2021-09-03 17:18:33 +0300

    Update Functional.js

--- front-end/src/Components/pages/competitionPage/Functional.js
+++ front-end/src/Components/pages/competitionPage/Functional.js
@@ -4,18 +4,14 @@ import { Navbar } from 'react-bootstrap';
 import LeftFunctional from './LeftFunctional';
 import RightFunctional from './RightFunctional';
 
-
 export default function Functional() {
     return (
-        <>
             <Navbar collapseOnSelect expand="md" className="grey type" >
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <LeftFunctional />
                     <RightFunctional />
-
                 </Navbar.Collapse>
             </Navbar>
-        </>
     )
 }
