0b865aacef85978149b60c363205c321b7123049
Maks Kovalchuk <87645543+Maksson@users.noreply.github.com>
2021-09-03 17:23:24 +0300

    Update RightBlock.js
    remove lines

--- front-end/src/Components/pages/competitionPage/RightBlock.js
+++ front-end/src/Components/pages/competitionPage/RightBlock.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Component } from 'react';
 
-
-
 class RightBlock extends Component {
     constructor(props) {
         super(props);
@@ -16,7 +14,9 @@ class RightBlock extends Component {
                 <Card bg="light" style={{ width: '15rem' }} >
                     <Card.Body style={{ height: '17rem' }}>
                         <Card.Img variant="top" style={{ height: '10rem' }} src={card.src} />
-                        <Card.Text className="text-center m-3" ><h5> {card.company_name} </h5>  </Card.Text>
+                        <Card.Text className="text-center m-3" >
+                            <h5> {card.company_name} </h5>  
+                        </Card.Text>
                     </Card.Body>
                 </Card>
             </>
