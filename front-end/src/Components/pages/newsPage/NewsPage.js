1f89cfefb8c91843f47cd73d4beb1f4a41f43052
ku4er.marina@gmail.com <ku4er.marina@gmail.com>
2021-09-06 22:41:24 +0300

    Grid system was fixed

--- front-end/src/Components/pages/newsPage/NewsPage.js
+++ front-end/src/Components/pages/newsPage/NewsPage.js
@@ -1,8 +1,7 @@
 import { Component } from "react"
-import { Container, Row, Navbar, Nav } from "react-bootstrap"
+import { Navbar } from "react-bootstrap"
 import CardRows from "./CardRows";
 import Footer from "../../Footer/Footer"
-
 
 
 export default class NewsPage extends Component {
@@ -12,15 +11,13 @@ export default class NewsPage extends Component {
                 <Navbar collapseOnSelect expand="md" className="grey">
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="justify-content-center ms-5">
-                            <Container className="justify-content-center ms-5 grey text-white">
-                                <h1 className="text-center m-3 type">NEWS</h1>
-                                <CardRows />
-                                <Footer />
-                            </Container>
-                        </Nav>
                     </Navbar.Collapse>
                 </Navbar>
+                <div className="grey text-white">
+                    <h1 className="text-center type">NEWS</h1>
+                    <CardRows />
+                    <Footer />
+                </div>
             </>
         )
     }
