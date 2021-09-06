29ef33d11a3a3de665fc591c3d5a3504f95881d1
ku4er.marina@gmail.com <ku4er.marina@gmail.com>
2021-09-06 22:38:18 +0300

    Some comments were added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Auth_Header from './Components/Header/Auth_Header';
 import Header from '../src/Components/Header/Header';
 
+
+{/* Auth_Header - use for registered users */ }
+{/* Header - use for unregistered users */ }
+
 function App() {
   return (
     <>
-      {/* <Auth_Header /> */}
-      <Header />
+      <Auth_Header />
+      {/* <Header /> */}
     </>
   );
 }
