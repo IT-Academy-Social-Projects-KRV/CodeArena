83bedab34981b933e303ccaf72d1f68d3fd05c0e
MrSancho42 <sashavoznuy@gmail.com>
2021-09-08 01:13:38 +0300

    created serializer for Task model

---
+++ codeArena/task/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from .models import Task, Language, Category
+
+
+class CreateTaskSerializer(serializers.ModelSerializer):
+    unit_test = serializers.FileField(
+        max_length=500, allow_null=True, allow_empty_file=True)
+
+    class Meta:
+        model = Task
+        exclude = ['_id', 'created_at', 'updated_at']
+
+    def validate_languages(self, value):
+        """
+        Checks for languages in the database.
+        """
+
+        language_list = [language.name
+                         for language in Language.objects.all()]
+
+        if all(language in language_list for language in value):
+            return value
+
+        raise serializers.ValidationError(
+            'One or more items do not exist in model Language.')
+
+    def validate_categories(self, value):
+        """
+        Checks for categories in the database.
+        """
+
+        category_list = [category.name
+                         for category in Category.objects.all()]
+
+        if all(category in category_list for category in value):
+            return value
+
+        raise serializers.ValidationError(
+            'One or more items do not exist in model Category.')
+
+    def create(self, validated_data):
+        return Task.objects.create(**validated_data)
