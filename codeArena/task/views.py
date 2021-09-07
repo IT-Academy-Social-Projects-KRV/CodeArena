47bd11dd8f17ad142ca7f79462e176c30b9863c3
MrSancho42 <sashavoznuy@gmail.com>
2021-09-08 01:22:28 +0300

    created view for CreateTaskSerializer

--- codeArena/task/views.py
+++ codeArena/task/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from django.db.utils import DatabaseError
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import CreateTaskSerializer
+
+
+class CreateTaskView(APIView):
+    def post(self, request, format='json'):
+        task = CreateTaskSerializer(data=request.data)
+
+        if task.is_valid():
+            try:
+                task.save()
+            except django.db.utils.DatabaseError:
+                return Response(
+                    status=status.HTTP_409_CONFLICT)
+
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
