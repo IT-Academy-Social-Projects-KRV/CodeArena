bc9beee60a2229d4419c22fa0a680b35f8b19951
Maksson <maxqwew@gmail.com>
2021-09-07 22:13:49 +0300

    Grid system was fixed on competitions page

--- front-end/src/Components/pages/competitionPage/MiddleBlock.js
+++ front-end/src/Components/pages/competitionPage/MiddleBlock.js
@@ -10,16 +10,14 @@ class MiddleBlock extends Component {
     render() {
         const { card } = this.props
         return (
-            <>
-                <Card bg="light" style={{ width: '54.6rem' }} >
-                    <Card.Body style={{ height: '17rem' }}>
-                        <Card.Title className="text-center type">
-                            Description of this competition
-                        </Card.Title>
-                        <Card.Text > <p> {card.description}  </p> </Card.Text>
-                    </Card.Body>
-                </Card>
-            </>
+            <Card bg="light" style={{ width: '42.5rem' }} >
+                <Card.Body style={{ height: 'auto' }}>
+                    <Card.Title className="text-center type">
+                        Description of this competition
+                    </Card.Title>
+                    <Card.Text > <p> {card.description}  </p> </Card.Text>
+                </Card.Body>
+            </Card>
         )
     }
 }
--- front-end/src/Components/pages/competitionPage/RightBlock.js
+++ front-end/src/Components/pages/competitionPage/RightBlock.js
@@ -10,16 +10,14 @@ class RightBlock extends Component {
     render() {
         const { card } = this.props
         return (
-            <>
-                <Card bg="light" style={{ width: '15rem' }} >
-                    <Card.Body style={{ height: '17rem' }}>
-                        <Card.Img variant="top" style={{ height: '10rem' }} src={card.src} />
-                        <Card.Text className="text-center m-3" >
-                            <h5> {card.company_name} </h5>  
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </>
+            <Card bg="light" style={{ width: '14.4rem' }} >
+                <Card.Body style={{ height: 'auto' }}>
+                    <Card.Img variant="top" style={{ height: '10rem' }} src={card.src} />
+                    <Card.Text className="text-center m-3" >
+                        <h5> {card.company_name} </h5>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         )
     }
 }
