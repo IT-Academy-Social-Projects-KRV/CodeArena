0df6e503732a5b6e94f96ec14eff0c0e254091d7
ku4er.marina@gmail.com <ku4er.marina@gmail.com>
2021-09-07 22:00:16 +0300

    News model fields added, migrations maked

--- codeArena/codeArena/urls.py
+++ codeArena/codeArena/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from django.conf.urls import url
+from django.conf.urls import include
+from news import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload/', views.add_news),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- codeArena/news/views.py
+++ codeArena/news/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import News
+from .forms import NewsForm
+
+
+def add_news(request):
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = NewsForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = NewsForm()
+    return render(request, 'index.html', {'form': form})
