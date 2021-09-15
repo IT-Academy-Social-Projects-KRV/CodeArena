b726911541578d80ba8d8bbe6a7ff92a23fb067e
Maksson <maxqwew@gmail.com>
2021-09-15 21:07:50 +0300

    vacancy

--- front-end/src/Components/pages/createVacancyPage/Step1.js
+++ front-end/src/Components/pages/createVacancyPage/Step1.js
@@ -5,7 +5,8 @@ export default class Step1 extends Component {
 
     render() {
         return (
-            <>  <h3 className="text-start ms-2">Step 1</h3>
+            <>
+                <h3 className="text-start ms-2">Step 1</h3>
                 <Form.Select className="my-3" aria-label="Default select example" >
                     <option>Choose language</option>
                     <option value="1">Java</option>
