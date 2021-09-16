bbaafcd3320b0dc6574ee6466d50107a1c5b7892
ku4er.marina@gmail.com <ku4er.marina@gmail.com>
2021-09-16 17:36:52 +0300

    urls path was added

--- codeArena/codeArena/urls.py
+++ codeArena/codeArena/urls.py
@@ -17,14 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-from django.conf.urls import url
-from django.conf.urls import include
+from django.conf.urls import url, include
 from news import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/news/', include('news.urls')),
     path('upload/', views.add_news),
 ]
 if settings.DEBUG:
