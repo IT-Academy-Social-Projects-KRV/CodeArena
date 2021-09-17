bb1241088af0ed7239617a10b50c2696d4530786
MrSancho42 <sashavoznuy@gmail.com>
2021-09-17 17:37:19 +0300

    fix importing views in urls

--- codeArena/task/urls.py
+++ codeArena/task/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateTaskView, GetTaskListView, GetTaskDetailView, GetLanguageListView, GetCategoryListView
+from .views import *
 
 
 app_name = 'task'
