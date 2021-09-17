bb10a8e4cf3b8ebe0e52cf3d77b8f64f1863816c
MrSancho42 <sashavoznuy@gmail.com>
2021-09-17 17:27:35 +0300

    added view for getting languages

--- codeArena/task/urls.py
+++ codeArena/task/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateTaskView, GetTaskListView, GetTaskDetailView
+from .views import CreateTaskView, GetTaskListView, GetTaskDetailView, GetLanguageListView
 
 
 app_name = 'task'
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create_task/', CreateTaskView.as_view()),
     path('get_task/', GetTaskListView.as_view()),
     path('get_task/<pk>/', GetTaskDetailView.as_view()),
+    path('get_language/', GetLanguageListView.as_view()),
 ]
