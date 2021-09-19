b04cc49cda2c4bfa41591eefbdd677c60d514795
dmytrosmetanin <dmytro.smetanin@gmail.com>
2021-09-19 22:33:05 +0300

    add the checker to prevent the try to manipulate with roles

--- codeArena/user/views.py
+++ codeArena/user/views.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers, status
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -7,12 +8,21 @@ from .serializers import UserSerializer
 class CreateUserAPIView(APIView):
     # Allow any user (authenticated or not) to access this url 
     permission_classes = (AllowAny,)
- 
+    
+
+    def validate_role_id(self, value):
+        if value in ["Moderator", "Admin"]:
+            raise ValidationError("U cannot ")
+    
+    
     def post(self, request):
         user = request.data
         serializer = UserSerializer(data=user)
         serializer.is_valid(raise_exception=True)
-        # if serializer.data #checkout wether they r not trying to create it with admin/moder
-        serializer.save()
+        if str(serializer.validated_data["role_id"]) in ["Admin", "Moderator"]: 
+            content = {"You can't be admin or moderator" : "please, provide the correct role!"}
+            return Response(content, status=status.HTTP_403_FORBIDDEN)
+        else:
+            serializer.save()
         
         return Response(serializer.data, status=status.HTTP_201_CREATED) #delete on production
