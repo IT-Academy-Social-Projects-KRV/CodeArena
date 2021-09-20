df1152a312533ce0af003fa34d36fa6be8476ad2
MrSancho42 <sashavoznuy@gmail.com>
2021-09-21 01:00:42 +0300

    chenged import in urls.py

--- codeArena/task/urls.py
+++ codeArena/task/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import *
+from . import views
 
 
 app_name = 'task'
 urlpatterns = [
-    path('create_task/', CreateTaskView.as_view()),
-    path('get_task/', GetTaskListView.as_view()),
-    path('get_task/<pk>/', GetTaskDetailView.as_view()),
-    path('get_language/', GetLanguageListView.as_view()),
-    path('get_category/', GetCategoryListView.as_view()),
+    path('create_task/', views.CreateTaskView.as_view()),
+    path('get_task/', views.GetTaskListView.as_view()),
+    path('get_task/<pk>/', views.GetTaskDetailView.as_view()),
+    path('get_language/', views.GetLanguageListView.as_view()),
+    path('get_category/', views.GetCategoryListView.as_view()),
 ]
