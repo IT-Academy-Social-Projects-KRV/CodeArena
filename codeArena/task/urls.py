307922a73f00ef27032e28e27ce6a4605c48528e
Oksana Lisovska <lisowska@i.ua>
2021-09-20 20:13:35 +0300

    add create view

--- codeArena/task/urls.py
+++ codeArena/task/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import CreateTaskView, GetTaskListView, GetTaskDetailView
-
+from . import views
 
 app_name = 'task'
 urlpatterns = [
     path('create_task/', CreateTaskView.as_view()),
     path('get_task/', GetTaskListView.as_view()),
     path('get_task/<pk>/', GetTaskDetailView.as_view()),
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create')
 ]
