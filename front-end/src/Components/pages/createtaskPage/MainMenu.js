9a63a3c5f209b31676cdf858230a46fac76f36d4
MrSancho42 <sashavoznuy@gmail.com>
2021-09-21 00:33:11 +0300

    renamed create task page files

---
+++ front-end/src/Components/pages/createtaskPage/MainMenu.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import LeftBlock from './LeftBlock';
+import RightBlock from './RightBlock';
+
+export default function MainMenu() {
+
+    return (
+        <>
+            <Row className="m-3">
+                <LeftBlock />
+                <RightBlock />
+            </Row>
+        </>
+    )
+}
