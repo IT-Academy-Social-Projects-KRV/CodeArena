3fe2b1eed685fbf50c9eff11e71e7c0193344315
ku4er.marina@gmail.com <ku4er.marina@gmail.com>
2021-09-28 03:17:06 +0300

    Documents were formatted

--- front-end/src/Components/pages/newsPage/CardRows.js
+++ front-end/src/Components/pages/newsPage/CardRows.js
@@ -4,37 +4,37 @@ import { Container, Row, Col } from "react-bootstrap";
 import CardNews from "./CardNews";
 
 class CardRows extends Component {
-  constructor() {
-    super();
-    this.state = {
-      news: [],
-    };
-  }
-
-  async componentDidMount() {
-    try {
-      const data = await axios.get("/api/news/");
-      this.setState({ news: data.data });
-    } catch (err) {
-      console.log(err);
+    constructor() {
+        super();
+        this.state = {
+            news: [],
+        };
     }
-  }
 
-  render() {
-    const { news } = this.state;
-    return (
-      <Container>
-        <Row>
-          {news.map((card) => {
-            return (
-              <Col md={3}>
-                <CardNews card={card} />
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    );
-  }
+    async componentDidMount() {
+        try {
+            const data = await axios.get("/api/news/");
+            this.setState({ news: data.data });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    render() {
+        const { news } = this.state;
+        return (
+            <Container>
+                <Row>
+                    {news.map((card) => {
+                        return (
+                            <Col md={3}>
+                                <CardNews card={card} />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+        );
+    }
 }
 export default CardRows;
--- front-end/src/Components/pages/userslistPage/RolesDropDown.js
+++ front-end/src/Components/pages/userslistPage/RolesDropDown.js
@@ -3,33 +3,33 @@ import React, { Component } from "react";
 import { Form } from "react-bootstrap";
 
 class RolesDropDown extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      roles: [],
-    };
-  }
-
-  async componentDidMount() {
-    try {
-      const rolesData = await axios.get("/api/user/roles/");
-      this.setState({ roles: rolesData.data });
-    } catch (error) {
-      console.log(error);
+    constructor(props) {
+        super(props);
+        this.state = {
+            roles: [],
+        };
     }
-  }
 
-  render() {
-    const { roles } = this.state;
-    return (
-      <Form.Select>
-        <option>Role</option>
-        {roles.map((role) => (
-          <option value={role.name}>{role.name}</option>
-        ))}
-      </Form.Select>
-    );
-  }
+    async componentDidMount() {
+        try {
+            const rolesData = await axios.get("/api/user/roles/");
+            this.setState({ roles: rolesData.data });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        const { roles } = this.state;
+        return (
+            <Form.Select>
+                <option>Role</option>
+                {roles.map((role) => (
+                    <option value={role.name}>{role.name}</option>
+                ))}
+            </Form.Select>
+        );
+    }
 }
 
 export default RolesDropDown;
