0d0a4e7bcfaad6cdfb2b1e18b41a7d3fe93dc5a7
ku4er.marina@gmail.com <ku4er.marina@gmail.com>
2021-10-05 02:59:19 +0300

    CoderTask model was changed, solution status was added

--- codeArena/task/models.py
+++ codeArena/task/models.py
@@ -29,7 +29,7 @@ class Task(models.Model):
     name = models.CharField(max_length=150, unique=True)
     description = models.TextField()
     user_id = models.UUIDField()
-    unit_test = models.FileField(max_length=500, upload_to="tests")
+    unit_test = models.TextField()
     rate = models.IntegerField()
     level = models.CharField(max_length=20)        # todo: choices
     status = models.CharField(
@@ -48,6 +48,14 @@ class Task(models.Model):
 
 
 class CoderTask(models.Model):
+    class SolutionStatus(models.TextChoices):
+        DRAFT = "DR"
+        EDITED = "ED"
+        CORRECT = "CR"
+        FAILED = "FL"
+        PUBLISHED = "PB"
+
+
     _id = models.ObjectIdField()
     coder_id = models.CharField(max_length=36, default='')
     task_id = models.ForeignKey(
@@ -55,5 +63,9 @@ class CoderTask(models.Model):
         on_delete=models.CASCADE,
     )
     solution = models.TextField()
-    status = models.BooleanField()
+    status = models.CharField(
+        max_length=2,
+        choices=SolutionStatus.choices,
+        default=SolutionStatus.DRAFT
+        )
 
