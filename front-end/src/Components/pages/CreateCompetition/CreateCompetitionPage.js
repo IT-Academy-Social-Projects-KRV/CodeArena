f87384890c80a0d2ca298134fb560c708cf2b58b
Viktorika-p <vika.akado@gmail.com>
2021-10-07 10:17:07 +0300

    connect API to create competition page

--- front-end/src/Components/pages/CreateCompetition/CreateCompetitionPage.js
+++ front-end/src/Components/pages/CreateCompetition/CreateCompetitionPage.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import axios from "axios";
 import Form from 'react-bootstrap/Form'
 import { Row, Col, Button } from 'react-bootstrap'
 import './createCompetitionPage.css';
@@ -9,12 +10,30 @@ import "react-datepicker/dist/react-datepicker.css";
 function CreateCompetitionPage() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState("");
+  const [formCompetition, setFormCompetition] = useState({
+    name: "",
+    start_time: "",
+    finish_time: "", 
+    description: ""
+  });
+  useEffect(() => {
+    console.log(formCompetition)
+  }, [formCompetition]);
   (function show_items () {
     window.onload = () => {
       document.querySelector(".competition_info_about").classList.add("show");
       document.querySelector(".competition_info_form").classList.add("show");
     }
   })()
+  let sendForm = async () => {
+    try {
+      const data = await axios.post("/competition/create_competition/", {...formCompetition, start_time:startDate, finish_time:endDate});
+
+
+    } catch (error) {
+      console.error(error);
+    }  
+  }
   return (
     <section className="competition_info">
       <div><h2>Create your competition</h2></div>
@@ -28,36 +47,26 @@ function CreateCompetitionPage() {
              in our form</div>
         </div>
         <div className="competition_info_form">
-        <Form>
+        <Form onSubmit={sendForm}>
           <Form.Group as={Col} controlId="formGridComprtition">
             <Form.Label>Competition</Form.Label>
-            <Form.Control type="Competition" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridCompany">
-            <Form.Label>Company</Form.Label>
-            <Form.Control type="Company" />
+            <Form.Control onChange={event => setFormCompetition({...formCompetition, name: event.target.value})} type="Competition" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formGridStart">
             <Form.Label>Start</Form.Label>
-            <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridtime">
-            <Form.Label>Time</Form.Label>
-            <Form.Control type="Company" />
+            <DatePicker selected={startDate} onChange={date => {setStartDate(date); setFormCompetition({...formCompetition, start_time: date.toLocaleDateString()})}} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formGridFinish">
             <Form.Label>Finish</Form.Label>
-            <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
+            <DatePicker selected={endDate} onChange={date => {setEndDate(date); setFormCompetition({...formCompetition, finish_time: date.toLocaleDateString()})}}/>
           </Form.Group>
 
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridDescription">
               <Form.Label>Description</Form.Label>
-              <Form.Control as="textarea" />
+              <Form.Control onChange={event => setFormCompetition({...formCompetition, description: event.target.value})} as="textarea" />
             </Form.Group>
           </Row>
 
@@ -73,8 +82,3 @@ function CreateCompetitionPage() {
 }
 
 export default CreateCompetitionPage;
-
-
-
-
-
