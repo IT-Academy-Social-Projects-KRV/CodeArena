6d59a32dd928976e1fadedbae633e186f2c22ee8
alexandr.v1111 <alexandr.v1111@gmail.com>
2021-10-11 11:47:12 +0300

    languages categories and search-by-name work

--- front-end/src/Components/pages/tasklistPage/LeftColumn.js
+++ front-end/src/Components/pages/tasklistPage/LeftColumn.js
@@ -8,7 +8,6 @@ import LanguagesDropDown from "../../Common/LanguagesDropDown";
 import CategoriesDropDown from "../../Common/CategoriesDropDown";
 import './s.css'
 
-// export default function LeftColumn() {
 export default class LeftColumn extends Component {
     render () {
         return (
@@ -27,44 +26,10 @@ export default class LeftColumn extends Component {
                                 <CategoriesDropDown className='category-choosing' overrideOnSelect={this.props.handleSelect} />
                             </Form.Group>
 
-
-                            <Form.Group style={{margin: '10px', marginBottom: '30px', }}>
-                                <Form.Label for='sort_by' > Sort by: </Form.Label>
-                                <Form.Select className='sort_by' aria-label="Default select example" style={{backgroundColor: '#292929', color: 'white'}}
-                                onChange={this.props.handleSortChange}> 
-                                <option value="1">Title</option>
-                                <option value="2">Popularity</option>
-                                <option value="3">Newest</option>
-                                <option value="4">Oldest</option>
-                                <option value="5">Hardest</option>
-                                <option value="6">Easiest</option>
-                                </Form.Select>
-                            </Form.Group>
-
-                            <Form.Group style={{margin: '10px', marginBottom: '30px', }}>
-                                <Form.Label for='searching-by-name' style={{color: 'white'}}> Search a task by the name 
-                                <Form.Control className='searching-by-name' placeholder='Enter the name of the task' 
-                                style={{backgroundColor: '#292929', color: 'white'}} value={this.props.value} onChange={this.props.handleSearchChange}/>
-                                </Form.Label>
-                            </Form.Group>
-
-                            {/* <Form.Group style={{margin: '10px', marginBottom: '30px'}}>
-                                <Form.Label for='langs' > choose language: </Form.Label>
-                                <Form.Select className='langs' aria-label="Default select example">
-                                <option value="1" onClick={() => this.setState({chosen_langs: this.state.chosen_langs.push('Python')})}>Python</option>
-                                <option value="2" onClick= { this.handleClick.bind(this, 'uiuiu')} >Ruby</option>
-                                <option value="3">JavaScript</option>
-                                </Form.Select>
-                            </Form.Group> */}
-
                         </Form>
                     </Card.Text>
-                    {/* <Button size="md" style={{ margin: '10px'}} variant="danger" onClick={this.attemptParams}>Attempt</Button> */}
-                    {/* <Button size="md" style={{ margin: '10px'}} variant="danger" onClick={this.props.attemptParams} language={this.la_id}>Attempt</Button> */}
                 </Card.Body>
             </Card>
         )
-
     }
-    
 }
